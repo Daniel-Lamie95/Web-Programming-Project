ba516bfd453eb6479ee6cff6e5ccdda11e6bba24
Daniel-Lamie95 <danielseddik95@gmail.com>
2026-04-26 22:03:23 +0300

    added student registration

---
+++ Config.php
@@ -0,0 +1,11 @@
+<?php
+	
+	$Host = 'localhost';
+	$username = 'root';
+	$password = '';
+	$DB = 'Library';
+	
+	$con = mysqli_connect($Host,$username,$password,$DB) or die (mysqli_error());
+	
+
+?>
