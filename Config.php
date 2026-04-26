cad30e942a0303cf66d13d30cd9f19b054493c61
Daniel-Lamie95 <danielseddik95@gmail.com>
2026-04-26 22:30:29 +0300

    config.php edit

--- Config.php
+++ Config.php
@@ -3,7 +3,7 @@
 	$Host = 'localhost';
 	$username = 'root';
 	$password = '';
-	$DB = 'Library';
+	$DB = 'APA_Project';
 	
 	$con = mysqli_connect($Host,$username,$password,$DB) or die (mysqli_error());
 	
