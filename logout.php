c892229169cd0df8ff11a6b6501871fef6120988
Judy Ahmed <ajudy1422@gmail.com>
2026-05-07 13:32:19 +0200

    add session unset to logout.php

--- logout.php
+++ logout.php
@@ -8,6 +8,8 @@ if (ini_get('session.use_cookies')) {
     setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
 }
 
+session_unset();
 session_destroy();
 header('Location: login.html');
 exit();
+?>
